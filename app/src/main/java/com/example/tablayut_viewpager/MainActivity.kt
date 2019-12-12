c5d70f6846f0297429446a78e7923d9733cc7c57
IN007848 <Reshma@oppo2019>
2019-12-12 15:57:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tablayut_viewpager/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.tablayut_viewpager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+
+class MainActivity : AppCompatActivity() {
+
+    var tabLayout: TabLayout? = null
+    var viewPager: ViewPager? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tabLayout = findViewById<TabLayout>(R.id.tabLayout)
+        viewPager = findViewById<ViewPager>(R.id.viewpager)
+
+        tabLayout!!.addTab(tabLayout!!.newTab().setText("Home"))
+        tabLayout!!.addTab(tabLayout!!.newTab().setText("Sport"))
+        tabLayout!!.addTab(tabLayout!!.newTab().setText("movie"))
+
+       tabLayout!!.tabGravity = TabLayout.GRAVITY_FILL
+        val adapter = MyAdapter(this,supportFragmentManager, tabLayout!!.tabCount )
+        viewPager!!.adapter=adapter
+
+viewPager!!.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabLayout))
+
+
+
+
+
+tabLayout!!.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+
+    override fun onTabSelected(tab: TabLayout.Tab?) {
+        viewPager!!.currentItem=tab!!.position
+    }
+
+
+
+    override fun onTabReselected(tab: TabLayout.Tab?) {
+
+    }
+
+    override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+    }
+
+
+
+})
+
+    }
+}
